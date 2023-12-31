8bef432685b29b51f71c26769714b751e2f19b67
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-12-31 18:35:49 +0000

    Remove support for specializing ToString outside the standard library
    This is the only trait specializable outside of the standard library.
    Before stabilizing specialization we will probably want to remove
    support for this. It was originally made specializable to allow a more
    efficient ToString in libproc_macro back when this way the only way to
    get any data out of a TokenStream. We now support getting individual
    tokens, so proc macros no longer need to call it as often.

--- compiler/rustc_span/src/lib.rs
+++ compiler/rustc_span/src/lib.rs
@@ -24,7 +24,6 @@
 #![feature(core_io_borrowed_buf)]
 #![feature(if_let_guard)]
 #![feature(let_chains)]
-#![feature(min_specialization)]
 #![feature(negative_impls)]
 #![feature(read_buf)]
 #![feature(round_char_boundary)]
